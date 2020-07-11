eb8b8ab367c8c2e8f6145fac5277d2fa00f5463b
Adam Chois <dragonboy174@gmail.com>
2020-07-11 12:32:27 -0700

    starting out with api setup

---
+++ my-weather-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const api = {
+  key: "3865eb84dbd9a101e25b1b9c3de22351",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
